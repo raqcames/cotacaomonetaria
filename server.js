a56124c1bb3cb39d9b511de3338b78fd74df8005
Glitch (hello-express) <none>
2021-02-23 18:35:42 +0000

    🍶🛤 Checkpoint ./server.js:23759820/1839

--- server.js
+++ server.js
@@ -23,9 +23,19 @@ app.get("/", (request, response) => {
 });
 
 // send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+app.get("https://economia.awesomeapi.com.br/json/all", (request, response) => {
   // express helps us take JS objects and send them as JSON
+  var teste = JSON.parse(body);
+  console.log("" + teste[''])
   response.json(dreams);
+});
+
+var request = require('request');
+request('http://api.openweathermap.org/data/2.5/weather?q=SaoPaulo&APPID=604b9d1c6b9e0b3e94b0b3ffd01fe6ba', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  var parsedWeather = JSON.parse(body);
+  console.log('A temperatura atual em São Paulo é ' + parsedWeather['main']['temp']); // Print the Temperature in the city of São Paulo
 });
 
 // listen for requests :)
