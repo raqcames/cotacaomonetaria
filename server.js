9bc9aee39ebcae201f8f59f60f795f54c45ab911
Glitch (hello-express) <none>
2021-02-23 19:22:47 +0000

    🛣🏨 Checkpoint ./package.json:23759820/30 ./server.js:23759820/710

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static("public"));
-
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+
+app.post('/Dialogflow', function(request, response){
+  var intentName = request.body.queryResult.intent.displayName;
+  
+  if(intentName == "Cotacao"){
+    response.json({"fulfillmentText": "Teste"})
+  }
+});
 
 app.get("https://economia.awesomeapi.com.br/json/all", (request, response) => {
   
